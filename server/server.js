c410b95f7ea47dcbd54105c09c8abf67899e87a3
keshariamudit24 <keshariamudit@gmail.com>
2025-06-10 13:55:15 +0530

    started backend implementation

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-// backend 
+const express = require("express")
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.listen(3000, () => { console.log(`listening on port: ${PORT}...`) });
+
